c38ca94f808352d103006e1e5c3f180cb7d7b922
Edward Zeng <edward.zeng.2009@smu.edu.sg>
2015-07-17 22:29:52 +0800

    created new functionality - subtract

--- src/test/java/model/CalculatorTest.java
+++ src/test/java/model/CalculatorTest.java
@@ -9,9 +9,14 @@ public class CalculatorTest {
     @Test
     public void correctFinalSumAfterAddingTwoNumbers() {
         Calculator calculator = new Calculator();
-        int finaSum = calculator.add(5, 5);
-        assertEquals(10, finaSum);
+        int finalSum = calculator.add(5, 5);
+        assertEquals(10, finalSum);
     }
 
-
+    @Test
+    public void correctFinalSumAfterSubtractingTwoNumbers() {
+        Calculator calculator = new Calculator();
+        int finalSum = calculator.subtract(5, 2);
+        assertEquals(3, finalSum);
+    }
 }
